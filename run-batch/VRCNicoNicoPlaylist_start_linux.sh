8951fdedb0e38c0c2760ce03e10b8b98de32df7c
Chino Nanami <tino_nanami@7mi.site>
2026-04-09 21:27:02 +0900

    update

--- run-batch/VRCNicoNicoPlaylist_start_linux.sh
+++ run-batch/VRCNicoNicoPlaylist_start_linux.sh
@@ -5,6 +5,17 @@ if [ -d ./tools ]; then
    ./tools/jdk-21.0.2/bin/java --module-path "./tools/javafx-sdk-21.0.10/lib" --add-modules javafx.controls,javafx.fxml -jar ./VRCNicoNicoPlaylistConverter-1.0-SNAPSHOT-all.jar
 else
   mkdir ./tools
+
+  curl https://www.7-zip.org/a/7z2501-linux-x64.tar.xz --output ./tools/7z2501-linux-x64.tar.xz
+  tar -Jxvf ./tools/7z2501-linux-x64.tar.xz
+  mkdir ./tools/7z2501
+  mv ./7zz ./tools/7z2501
+  mv ./7zzs ./tools/7z2501
+  mv ./History.txt ./tools/7z2501
+  mv ./License.txt ./tools/7z2501
+  mv ./MANUAL ./tools/7z2501
+  mv ./readme.txt ./tools/7z2501
+  chmod +x ./tools/7z2501/7zz
   
   curl https://download.java.net/java/GA/jdk21.0.2/f2283984656d49d69e91c558476027ac/13/GPL/openjdk-21.0.2_linux-x64_bin.tar.gz --output ./tools/openjdk-21.0.2_linux-x64_bin.tar.gz
   tar -zxvf ./tools/openjdk-21.0.2_linux-x64_bin.tar.gz
@@ -12,8 +23,10 @@ else
   chmod +x ./tools/jdk-21.0.2/bin/java
 
   curl https://download2.gluonhq.com/openjfx/21.0.10/openjfx-21.0.10_linux-x64_bin-sdk.zip --output ./tools/openjfx-21.0.10_linux-x64_bin-sdk.zip
-  tar -xf ./tools/openjfx-21.0.10_linux-x64_bin-sdk.zip
+  ./tools/7z/7zz x ./tools/openjfx-21.0.10_linux-x64_bin-sdk.zip
   mv ./javafx-sdk-21.0.10 ./tools
+
+  rm -fr /tools/7z2501
 
   ./tools/jdk-21.0.2/bin/java --module-path "./tools/javafx-sdk-21.0.10/lib" --add-modules javafx.controls,javafx.fxml -jar ./VRCNicoNicoPlaylistConverter-1.0-SNAPSHOT-all.jar
   
