59ebef7bb17d17b8f405e29bde7ae90272e5b5f1
Chino Nanami <tino_nanami@7mi.site>
2026-04-09 21:28:36 +0900

    update

--- run-batch/VRCNicoNicoPlaylist_start_linux.sh
+++ run-batch/VRCNicoNicoPlaylist_start_linux.sh
@@ -6,16 +6,16 @@ if [ -d ./tools ]; then
 else
   mkdir ./tools
 
-  curl https://www.7-zip.org/a/7z2501-linux-x64.tar.xz --output ./tools/7z2501-linux-x64.tar.xz
-  tar -Jxvf ./tools/7z2501-linux-x64.tar.xz
-  mkdir ./tools/7z2501
-  mv ./7zz ./tools/7z2501
-  mv ./7zzs ./tools/7z2501
-  mv ./History.txt ./tools/7z2501
-  mv ./License.txt ./tools/7z2501
-  mv ./MANUAL ./tools/7z2501
-  mv ./readme.txt ./tools/7z2501
-  chmod +x ./tools/7z2501/7zz
+  curl https://github.com/ip7z/7zip/releases/download/26.00/7z2600-linux-x64.tar.xz --output ./tools/7z2600-linux-x64.tar.xz
+  tar -Jxvf ./tools/7z2600-linux-x64.tar.xz
+  mkdir ./tools/7z2600
+  mv ./7zz ./tools/7z2600
+  mv ./7zzs ./tools/7z2600
+  mv ./History.txt ./tools/7z2600
+  mv ./License.txt ./tools/7z2600
+  mv ./MANUAL ./tools/7z2600
+  mv ./readme.txt ./tools/7z2600
+  chmod +x ./tools/7z2600/7zz
   
   curl https://download.java.net/java/GA/jdk21.0.2/f2283984656d49d69e91c558476027ac/13/GPL/openjdk-21.0.2_linux-x64_bin.tar.gz --output ./tools/openjdk-21.0.2_linux-x64_bin.tar.gz
   tar -zxvf ./tools/openjdk-21.0.2_linux-x64_bin.tar.gz
@@ -26,7 +26,7 @@ else
   ./tools/7z/7zz x ./tools/openjfx-21.0.10_linux-x64_bin-sdk.zip
   mv ./javafx-sdk-21.0.10 ./tools
 
-  rm -fr /tools/7z2501
+  rm -fr /tools/7z2600
 
   ./tools/jdk-21.0.2/bin/java --module-path "./tools/javafx-sdk-21.0.10/lib" --add-modules javafx.controls,javafx.fxml -jar ./VRCNicoNicoPlaylistConverter-1.0-SNAPSHOT-all.jar
   
